818891bd0e7c02cf98fb287a09dfc6f21e14cbbb
binhnd15107 <115780370+binhndvgsholding@users.noreply.github.com>
2022-10-30 23:42:48 +0700

    update code

--- scr/public/asset/js/socket.io.js
+++ scr/public/asset/js/socket.io.js
@@ -1,39 +1,37 @@
-$(document).ready(function(){ 
-  socket.on('connect', ()=>{
-      socket.emit('user_connect',user_id);
-  })
+$(document).ready(function () {
+  socket.on("connect", () => {
+    socket.emit("user_connect", user_id);
+  });
 
-   socket.on('updateUserStatus', (users)=>{
-    let useStatusIcon = $('.user-status-icon');
-    let useStatusText = $('.user-status-text');
+  socket.on("updateUserStatus", (users) => {
+    let useStatusIcon = $(".user-status-icon");
+    let useStatusText = $(".user-status-text");
     // useStatusIcon.removeClass('online_icon');
-      useStatusIcon.addClass('offline')
-      useStatusText.html('<b>Offline</b>')
-      $.each(users, function(key, value){
-        if(value !=null && value != 0){
-          let userIcon = $('.user-icon-'+key)
-          let userText = $('.user-text-'+key)
-            userIcon.removeClass('offline');
-            userText.html('<b>Online</b>')
-        }
-      })
-  })
+    useStatusIcon.addClass("offline");
+    useStatusText.html("<b>Offline</b>");
+    $.each(users, function (key, value) {
+      if (value != null && value != 0) {
+        let userIcon = $(".user-icon-" + key);
+        let userText = $(".user-text-" + key);
+        userIcon.removeClass("offline");
+        userText.html("<b>Online</b>");
+      }
+    });
+  });
 
   // send mess private len serve
-  $('.send-mess-user').click(function(){
-    var mess = $('textarea[name=messagesUser]').val();
-    var receiverId =$('textarea[name=messagesUser]').data('id')
-    socket.emit('send_message_private',
-     {
-        sender_id : user_id,
-        receiver_img :user_img,
-        receiver_id:receiverId,
-        content: mess,
-        created_at: new Date()
-     })
-     var showMess = "";
-     showMess += 
-     `<div class="d-flex justify-content-end mb-4">
+  $(".send-mess-user").click(function () {
+    var mess = $("textarea[name=messagesUser]").val();
+    var receiverId = $("textarea[name=messagesUser]").data("id");
+    socket.emit("send_message_private", {
+      sender_id: user_id,
+      receiver_img: user_img,
+      receiver_id: receiverId,
+      content: mess,
+      created_at: new Date(),
+    });
+    var showMess = "";
+    showMess += `<div class="d-flex justify-content-end mb-4">
      <div class="msg_cotainer_send">
          ${mess}
          <span class="msg_time_send"></span>
@@ -42,18 +40,18 @@ $(document).ready(function(){
         <img src="/img/${user_img}" class="rounded-circle user_img_msg">
      </div>
     </div>`;
-     document.querySelector('.show-mess-user-private').innerHTML +=showMess
-    
-     $('textarea[name=messagesUser]').val('');
+    document.querySelector(".show-mess-user-private-" + receiverId).innerHTML +=
+      showMess;
 
-     return false
-    })
-  
-    //listen form serve
-    socket.on('new_message_private',(data)=>{
-        var showMess = "";
-     showMess += 
-     `<div class="d-flex justify-content-start mb-4">
+    $("textarea[name=messagesUser]").val("");
+
+    return false;
+  });
+
+  //listen form serve
+  socket.on("new_message_private", (data) => {
+    var showMess = "";
+    showMess += `<div class="d-flex justify-content-start mb-4">
      <div class="img_cont_msg">
          <img src="/img/${data.receiver_img}" class="rounded-circle user_img_msg">
      </div>   
@@ -62,8 +60,8 @@ $(document).ready(function(){
          <span class="msg_time"></span>
      </div>
      </div>`;
-     document.querySelector('.show-mess-user-private').innerHTML +=showMess
-    })
-  
- 
-})
+    document.querySelector(
+      ".show-mess-user-private-" + data.sender_id
+    ).innerHTML += showMess;
+  });
+});
